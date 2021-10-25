006bee1a99fc53094135cd7b4b97d89bd1e74b47
Max Lavrenov <maxim@btcx.com>
2021-10-25 18:49:51 +0200

    fix path in statusline

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -3,6 +3,12 @@ local components = require('feline.presets.default')
 local lsp_status = require('lsp-status');
 
 
+
+-- modify file_info component
+components.active[1][3].opts = {
+	type = 'relative',
+}
+
 components.active[1][10] = {
 	provider = function () 
 		return lsp_status.status_progress()
